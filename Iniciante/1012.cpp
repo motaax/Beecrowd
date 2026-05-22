12df68220e0810c491497d4933ca436e5d2b85ad
Felipe Mota <felipemota465@gmail.com>
2026-05-22 13:09:39 -0300

    Iniciante

---
+++ Iniciante/1012.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double A, B, C;
+    const double pi = 3.14159;
+
+    cin >> A >> B >> C;
+
+    double triangulo = (A * C) / 2.0;
+    double circulo = pi * C * C;
+    double trapezio = ((A + B) * C) / 2.0;
+    double quadrado = B * B;
+    double retangulo = A * B;
+
+    cout << fixed << setprecision(3);
+
+    cout << "TRIANGULO: " << triangulo << endl;
+    cout << "CIRCULO: " << circulo << endl;
+    cout << "TRAPEZIO: " << trapezio << endl;
+    cout << "QUADRADO: " << quadrado << endl;
+    cout << "RETANGULO: " << retangulo << endl;
+
+    return 0;
+}
