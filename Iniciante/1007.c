0ed7493e572e6fd576410d063dd8b2ff9d75e496
Felipe Mota <felipemota465@gmail.com>
2026-05-22 10:44:09 -0300

    Iniciante

---
+++ Iniciante/1007.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C, D;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    scanf("%d", &C);
+    scanf("%d", &D);
+
+    int diferenca = (A * B) - (C * D);
+
+    printf("DIFERENCA = %d\n", diferenca);
+
+    return 0;
+}
